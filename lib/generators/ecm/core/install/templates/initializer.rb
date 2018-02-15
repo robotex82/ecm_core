f437b848d6a3fd1c1174623fe35abfdfcc268385
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2018-02-15 22:25:19 +0100

    Add missing newline in generated routes. Make base controller class name configurable in the install generator via BASE_CONTROLLER_CLASS_NAME option.

--- lib/generators/ecm/core/install/templates/initializer.rb
+++ lib/generators/ecm/core/install/templates/initializer.rb
@@ -1,7 +1,7 @@
 Ecm::Core.configure do |config|
   # Set the base controller for the contact form
   #
-  # Default: config.base_controller = '::FrontendController'
+  # Default: config.base_controller = '<%= base_controller_class_name %>'
   #
-  config.base_controller = '::FrontendController'
+  config.base_controller = '<%= base_controller_class_name %>'
 end
